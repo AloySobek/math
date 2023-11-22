20f8790ca739ae2f7a3cbce238603aed36ccf0a5
AloySobek <supernova.wandering.star@gmail.com>
2023-11-22 23:55:44 +0100

    chore: still figuring out the project purposes and structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ParallelProgrammingTraining
-No description
+# math
+
+This is an educational library where I try to implement various mathematical concepts
+and at the same time try to explain those concepts in a fun way.
+
+This is not a production ready math library and I'm not aiming for it to be like that.
+It's just a fun personal project which I hope will teach me and potentially somebody else
--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,65 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: Rustam <super.rustamm@gmail.com>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/28 15:09:24 by Rustam            #+#    #+#             */
-/*   Updated: 2019/12/19 20:09:40 by Rustam           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
 
-#include "main.hpp"
+template <typename T> class Vector {
+  private:
+    T x;
+    T y;
+    T z;
+};
 
-#define SIZE 10000000
+int main(int argc, char **argv) {
+    std::cout << "WTF" << std::endl;
 
-void	*checkX(void *ptr)
-{
-	if (((char *)ptr)[0] == 0)
-	{
-		((char *)ptr)[0] += 2;
-		((char *)ptr)[0] -= 2;
-	}
-	return (NULL);
+    return 0;
 }
 
-void	*checkY(void *ptr)
-{
-	if (((char *)ptr)[1] == 0)
-	{
-		((char *)ptr)[1] += 3;
-		((char *)ptr)[1] -= 3;
-	}
-	return (NULL);
-}
-
-int	main(int argc, char **argv)
-{
-	pthread_t		one, two;
-	char			*test;
-
-	test = (char *)malloc(SIZE);
-	if (!test)
-		return (0);
-	memset(test, 0, SIZE);
-	pthread_create(&one, NULL, checkX, (void *)test);
-	pthread_create(&two, NULL, checkY, (void *)test);
-	for (int i = 0; i < 10000000; ++i)
-	{
-		pthread_join(one, NULL);
-		pthread_join(two, NULL);
-	}
-	//Vector one(3.0, 4.0, 5.0, 0.0);
-	//Vector two(11.0, 14.5, 5.0, 0.0);
-	//double res;
-	//glm::vec4 one1(3.0, 4.0, 5.0, 0.0);
-	//glm::vec4 two1(11.0, 14.5, 5.0, 0.0);
-
-	//std::cout << Vector(1.0, 2.0, 3.0, 0.0) << std::endl;
-
-	//for (int i = 0; i < 100000000; ++i)
-	//	res = one(two);
-	//std::cout << one(two) << std::endl;
-	//for (int i = 0; i < 100000000; ++i)
-	//	res = glm::dot(one1, two1);
-	//std::cout << glm::dot(one1, two1) << std::endl;
-    return (0);
-}
+// #include "main.hpp"
+//
+// #define SIZE 10000000
+//
+// void *checkX(void *ptr) {
+//     if (((char *)ptr)[0] == 0) {
+//         ((char *)ptr)[0] += 2;
+//         ((char *)ptr)[0] -= 2;
+//     }
+//     return (NULL);
+// }
+//
+// void *checkY(void *ptr) {
+//     if (((char *)ptr)[1] == 0) {
+//         ((char *)ptr)[1] += 3;
+//         ((char *)ptr)[1] -= 3;
+//     }
+//     return (NULL);
+// }
+//
+// int main(int argc, char **argv) {
+//     pthread_t one, two;
+//     char *test;
+//
+//     test = (char *)malloc(SIZE);
+//     if (!test)
+//         return (0);
+//     memset(test, 0, SIZE);
+//     pthread_create(&one, NULL, checkX, (void *)test);
+//     pthread_create(&two, NULL, checkY, (void *)test);
+//     for (int i = 0; i < 10000000; ++i) {
+//         pthread_join(one, NULL);
+//         pthread_join(two, NULL);
+//     }
+//     // Vector one(3.0, 4.0, 5.0, 0.0);
+//     // Vector two(11.0, 14.5, 5.0, 0.0);
+//     // double res;
+//     // glm::vec4 one1(3.0, 4.0, 5.0, 0.0);
+//     // glm::vec4 two1(11.0, 14.5, 5.0, 0.0);
+//
+//     // std::cout << Vector(1.0, 2.0, 3.0, 0.0) << std::endl;
+//
+//     // for (int i = 0; i < 100000000; ++i)
+//     //	res = one(two);
+//     // std::cout << one(two) << std::endl;
+//     // for (int i = 0; i < 100000000; ++i)
+//     //	res = glm::dot(one1, two1);
+//     // std::cout << glm::dot(one1, two1) << std::endl;
+//     return (0);
+// }
