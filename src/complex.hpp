59d03613fd06b3f86035e64f43b468e81772a7b4
AloySobek <supernova.wandering.star@gmail.com>
2024-03-29 14:36:29 +0100

    Raylib added as project dependency

--- src/complex.hpp
+++ src/complex.hpp
@@ -1,9 +1,11 @@
 #include <type_traits>
+
 // A complex number is an ordered pair (a,b) where a and b ∈ R
 // The set of all complex numbers is denoted as C
 template <typename T> class Complex {
   public:
-    Complex(T a, T b) : _a(a), _b(b) {}
+    Complex() : _a{0}, _b{0} {}
+    Complex(T a, T b) : _a{a}, _b{b} {}
     Complex(const Complex<T> &other) {
         if (&other != this) {
             _a = other._a;
@@ -17,19 +19,17 @@ template <typename T> class Complex {
             _b = other._b;
         }
 
-        return this;
+        return *this;
     }
 
-    bool operator==(const Complex<T> &other) const { return _a == other._a && _b == other._b; }
-
-    // Let a be a complex number (x, y) or (x + yi)
-    // Let b be a complex number (z, w) or (z + wi)
-    // Then addition of complex is defined by:
-    // (x + yi) + (z + wi) = (x + z) + (y + w)i
-    // Notice how the resulting addition is by itself a complex number:
-    // Let λ be a complex number (x + z, y + w) or ((x + z) + (y + w)i)
-    // let n = x + z and m = y + w, then λ = (n, m) or (n + mi)
+    // A = B -> (a + bi) = (c + di) -> a = c and b = d
+    bool operator==(const Complex<T> &b) const { return _a == b._a && _b == b._b; }
+    // A + B = (a + bi) + (c + di) = (a + c) + (bi + di) = (a + c) + (b + d)i
     Complex<T> operator+(const Complex<T> &b) const { return Complex<T>(_a + b._a, _b + b._b); }
+    // A * B = (a + bi)(c + di) = ac + (ad + bc)i - bd = (ac - bd) + (ad + bc)i; i^2 = -1
+    Complex<T> operator*(const Complex<T> &b) const {
+        return Complex<T>((_a * b._a - _b * b._b), (_a * b._b + _b * b._a));
+    }
 
     T a() const { return _a; }
     T b() const { return _b; }
---
+++ test/test_complex.hpp
@@ -0,0 +1,105 @@
+#include <doctest.h>
+#include <iostream>
+#include <random>
+
+#include "complex.hpp"
+
+TEST_CASE("Complex number initialization") {
+    Complex<int> c = Complex<int>(3, 5);
+
+    bool check = c == Complex<int>(3, 5) && c.a() == 3 && c.b() == 5;
+
+    CHECK(check);
+
+    Complex<Complex<int>> cc = Complex<Complex<int>>(Complex<int>(3, 5), Complex<int>(7, 15));
+
+    check = cc == Complex<Complex<int>>(Complex<int>(3, 5), Complex<int>(7, 15)) &&
+            cc.a() == Complex<int>(3, 5) && cc.b() == Complex<int>(7, 15);
+
+    CHECK(check);
+}
+
+TEST_CASE("Complex number addition") {
+    Complex<int> sum = Complex<int>(3, 5) + Complex<int>(7, 15);
+
+    bool check = sum == Complex<int>(10, 20) && sum.a() == 10 && sum.b() == 20;
+
+    CHECK(check);
+
+    sum = Complex<int>(0, 0) + Complex<int>(0, 0);
+
+    check = sum == Complex<int>(0, 0) && sum.a() == 0 && sum.b() == 0;
+
+    CHECK(check);
+
+    sum = Complex<int>(-3, -5) + Complex<int>(-7, -15);
+
+    check = sum == Complex<int>(-10, -20) && sum.a() == -10 && sum.b() == -20;
+
+    CHECK(check);
+}
+
+TEST_CASE("Complex number multiplication") {
+    Complex<int> product = Complex<int>(3, 7) * Complex<int>(11, 17);
+
+    bool check = product == Complex<int>(-86, 128) && product.a() == -86 && product.b() == 128;
+
+    CHECK(check);
+
+    product = Complex<int>(0, 0) * Complex<int>(0, 0);
+
+    check = product == Complex<int>(0, 0) && product.a() == 0 && product.b() == 0;
+
+    CHECK(check);
+
+    product = Complex<int>(-3, -7) * Complex<int>(-11, -17);
+
+    check = product == Complex<int>(-86, 128) && product.a() == -86 && product.b() == 128;
+
+    CHECK(check);
+}
+
+TEST_CASE("Random complex number addition") {
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(-100, 100);
+
+    for (int i = 0; i < 10; ++i) {
+        int a = distr(gen);
+        int b = distr(gen);
+        int c = distr(gen);
+        int d = distr(gen);
+
+        Complex<int> alpha = Complex<int>(a, b);
+        Complex<int> beta = Complex<int>(c, d);
+
+        Complex<int> sum = alpha + beta;
+
+        bool check = sum.a() == a + c && sum.b() == b + d;
+
+        CHECK(check);
+    }
+}
+
+TEST_CASE("Random complex number addition") {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(-100, 100);
+
+    for (int i = 0; i < 10; ++i) {
+        int a = distr(gen);
+        int b = distr(gen);
+        int c = distr(gen);
+        int d = distr(gen);
+
+        Complex<int> alpha = Complex<int>(a, b);
+        Complex<int> beta = Complex<int>(c, d);
+
+        Complex<int> product = alpha * beta;
+
+        bool check = product.a() == (a * c - b * d) && product.b() == (a * d + b * c);
+
+        CHECK(check);
+    }
+}
